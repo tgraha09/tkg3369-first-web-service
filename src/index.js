1d7bf495367669f6204d4703578493da03ab3cf9
tgraha09 <tfire09@gmail.com>
2021-09-06 09:18:21 -0400

    Added eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-console.log("First web service starting up ...");
-
+console.log('First web service starting up ...');
+// npm i --save-dev eslint eslint-config-airbnb eslint-plugin-import
+const name = 'fred';
+const car = {
+  make: 'Ford',
+  otherMake: 'Ford',
+};
 // 1 - pull in the HTTP server module
 const http = require('http');
 
 // 2 - pull in URL and query modules (for URL parsing)
-const url = require('url')
+const url = require('url');
 const query = require('querystring');
-const path = require('path');
+// const path = require('path');
 
 // 3 - locally this will be 3000, on Heroku it will be assigned
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
@@ -39,78 +44,56 @@ const errorPage = `<html>
   </p>
 </body>
 </html>
-`
-const getRandomNumberJSON = (max=1)=>{
-    max = Number(max)
-    max = !max ? 1 : max
-    max = max < 1 ? 1 : max
-    const number = Math.random()* max;
-    const responseObj = {
-        timestamp: new Date(),
-        number: number
-    }
-    return JSON.stringify(responseObj)
+`;
+const getRandomNumberJSON = (m = 1) => {
+  let max = m;
+  max = Number(max);
+  max = !max ? 1 : max;
+  max = max < 1 ? 1 : max;
+  const number = Math.random() * max;
+  const responseObj = {
+    timestamp: new Date(),
+    number,
+  };
+  return JSON.stringify(responseObj);
 };
-const randomPage = (val) =>{
-    return `<html>
-    <head>
-      <title>Random Number JSON</title>
-    </head>
-    <body>
-      <h1>Random Number JSON</h1>
-      <p> ${getRandomNumberJSON(val)}
-      </p>
-      
-    </body>
-    </html>
-    `
-}
-
-
-
 
 // 6 - this will return a random number no bigger than `max`, as a string
 // we will also doing our query parameter validation here
-
-
 
 // 7 - this is the function that will be called every time a client request comes in
 // this time we will look at the `pathname`, and send back the appropriate page
 // note that in this course we'll be using arrow functions 100% of the time in our server-side code
 const onRequest = (request, response) => {
-    //console.log(request.headers);
-    const parsedUrl = url.parse(request.url)
-    const pathname = parsedUrl.pathname
-    console.log("parsedUrl=", parsedUrl);
-    console.log("pathname=", pathname);
+  // console.log(request.headers);
+  const parsedUrl = url.parse(request.url);
+  const { pathname } = parsedUrl;
+  // console.log('parsedUrl=', parsedUrl);
+  // console.log('pathname=', pathname);
 
-    if(pathname == "/"){
-        //console.log("Found");
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        response.write(indexPage);
-        response.end()
-    }
-    else if (pathname == "/random-number"){
-        const params = query.parse(parsedUrl.query);
-        const max = params.max;
-        console.log("params=", params);
-        console.log("max=", max);
-        //const params = Object.fromEntries(urlSearchParams.entries());
-        //console.log(params);
-        //console.log(JSON.parse(parsedUrl.query));
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        response.write(getRandomNumberJSON(max));
-        response.end()
-    }
-    else{
-        response.writeHead(404, {'Content-Type': 'text/html'})
-        response.write(errorPage);
-        response.end()
-    }
-    
+  if (pathname === '/') {
+    // console.log("Found");
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(indexPage);
+    response.end();
+  } else if (pathname === '/random-number') {
+    const params = query.parse(parsedUrl.query);
+    const { max } = params;
+    // console.log('params=', params);
+    // console.log('max=', max);
+    // const params = Object.fromEntries(urlSearchParams.entries());
+    // console.log(params);
+    // console.log(JSON.parse(parsedUrl.query));
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(getRandomNumberJSON(max));
+    response.end();
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.write(errorPage);
+    response.end();
+  }
 };
 
-
 // 8 - create the server, hook up the request handling function, and start listening on `port`
-http.createServer(onRequest).listen(port)
+http.createServer(onRequest).listen(port);
 console.log(`Listening on 127.0.0.1: ${port}`);
